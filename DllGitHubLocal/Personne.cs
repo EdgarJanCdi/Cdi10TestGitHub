a2ead570eb43aea86d59b5b5a20cd31c4536396a
Edgar <edgar.safaryan@2isa.org>
2018-11-08 16:45:29 +0100

    Initial

---
+++ DllGitHubLocal/Personne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllGitHubLocal
+{
+    public class Personne
+    {
+        private int _Age;
+
+        public int Age
+        {
+            get { return _Age; }
+            set { _Age = value; }
+        }
+    }
+}
---
+++ Testeur/Program.cs
@@ -0,0 +1,20 @@
+﻿using DllGitHubLocal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testeur
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne p1 = new Personne();
+            p1.Age = 21;
+            Console.WriteLine("L'age est : " + p1.Age);
+            Console.ReadKey();
+        }
+    }
+}
